b3fb6f288e5a436d27ff1da178b5bee88016d5dc
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-02-01 23:47:55 +0500

    5-Updation

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 
+
 const app = express();  // is called function expression
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+})
+
+app.get('/about',(req,res)=>{
+    res.send('About Page');
+})
+
+
 
 app.listen(3000);
